18d5170f7187cf81dd4b51083b05103eb4b0100e
Albert Drenth <2001drenth@gmail.com>
2025-05-27 15:43:30 +0200

    reclame en algemene_functies toegevoegd.

---
+++ algemene_functies.py
@@ -0,0 +1,10 @@
+def mijn_functie_1(keer):
+    return keer * keer
+
+def mijn_functie_2(argumenten, teruggeefwaarde):
+    return [
+        argumenten + teruggeefwaarde,
+        argumenten - teruggeefwaarde,
+        argumenten * teruggeefwaarde,
+        argumenten / teruggeefwaarde
+    ]
---
+++ reclame.py
@@ -0,0 +1,21 @@
+def aanbieding_1(smaak, prijs, korting):
+    nieuwe_prijs = prijs * (1 - korting)
+    return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {nieuwe_prijs:.2f} euro."
+
+def inkomsten_totaal(inkomsten, btw=0.09):
+    totaal = sum(inkomsten)
+    btw_bedrag = totaal * btw
+    return f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag:.2f} euro btw betaald dient te worden."
+
+def laag_en_hoog(mijn_lijst):
+    hoogste = max(mijn_lijst)
+    laagste = min(mijn_lijst)
+    return [hoogste, laagste]
+
+def gemiddelde(mijn_lijst):
+    gemiddelde_bedrag = sum(mijn_lijst) / len(mijn_lijst)
+    return f"De gemiddelde inkomsten deze week zijn {gemiddelde_bedrag:.2f} euro."
+
+def meervoudig(invoer_lijst)
+    return laag_en_hoog(invoer_lijst)
+
