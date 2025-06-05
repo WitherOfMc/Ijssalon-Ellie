5d51cb5c55d4d98a7187a69c778cc22f6f51be53
Albert Drenth <2001drenth@gmail.com>
2025-06-05 22:03:43 +0200

    Helper geupdate

--- helper.py
+++ helper.py
@@ -1,10 +1,11 @@
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
+def decoreer(tekst=""):
+    tekst = "header"
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
 
 def fooi_pp(bedrag, personen):
     try:
@@ -13,7 +14,8 @@ def fooi_pp(bedrag, personen):
          bedrag_pp = "??"
     return f"Het bedrag per persoon is {bedrag_pp} euro"
 
-b = int(input("Welk bedrag zit er in de fooienpot? "))
-p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
-
-print(fooi_pp(b,p))
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
